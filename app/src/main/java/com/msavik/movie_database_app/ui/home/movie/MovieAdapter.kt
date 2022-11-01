6cdd7276b513ce9c4f0240c3cc91490fd2d962f5
Marko Savic <xm.marko@gmail.com>
2022-11-01 13:28:01 +0100

    Add Glide for image loading Modify list UI

--- app/src/main/java/com/msavik/movie_database_app/ui/home/movie/MovieAdapter.kt
+++ app/src/main/java/com/msavik/movie_database_app/ui/home/movie/MovieAdapter.kt
@@ -5,8 +5,14 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.msavik.data.utility.BaseUrl
 import com.msavik.domain.model.movie.Movie
 import com.msavik.movie_database_app.databinding.ItemMovieDetailsBinding
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MovieAdapter : ListAdapter<Movie, MovieAdapter.ViewHolder>(ItemDiffCallback()) {
 
@@ -25,7 +31,16 @@ class MovieAdapter : ListAdapter<Movie, MovieAdapter.ViewHolder>(ItemDiffCallbac
     ) : RecyclerView.ViewHolder(binding.root) {
         open fun bindTo(movie: Movie) {
             binding.apply {
+                Glide.with(this@ViewHolder.itemView.context)
+                    .load(BaseUrl.BASE_IMAGE_URL + movie.poster_path)
+                    .diskCacheStrategy(DiskCacheStrategy.DATA)
+                    .into(ivMovie)
+
                 tvTitle.text = movie.title
+                tvReleaseDate.text = DateFormat.getDateInstance().format(
+                    SimpleDateFormat("yyyy-mm-dd", Locale.ENGLISH).parse(movie.release_date) ?: ""
+                )
+                tvRating.text = "${movie.vote_average.toString()} / 10"
             }
         }
     }
--- data/src/main/java/com/msavik/data/utility/Constants.kt
+++ data/src/main/java/com/msavik/data/utility/Constants.kt
@@ -2,6 +2,6 @@ package com.msavik.data.utility
 
 object BaseUrl {
     const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
-    const val BASE_IMAGE_URL = "https://api.themoviedb.org/t/p/original"
+    const val BASE_IMAGE_URL = "https://www.themoviedb.org/t/p/original"
     const val TMDB_API_KEY = "1f398bce243b5b0b03328a16ad50f7d2"
 }
